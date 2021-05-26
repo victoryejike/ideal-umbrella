471304bbd8d768a52c8c9b8b4cfdc503155eb45a
Ken So <sotszyau312@gmail.com>
2021-05-27 03:32:15 +0800

    style: Config Global CSS
    Signed-off-by: Ken So <sotszyau312@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,66 @@
 <template>
-  <router-view />
+  <div id="wrapper">
+    <AppHeader id="header" />
+    <div id="content">
+      <router-view />
+    </div>
+  </div>
+  <AppFooter id="footer" />
 </template>
 
+<script>
+import AppHeader from '@/components/Layout/AppHeader.vue';
+import AppFooter from '@/components/Layout/AppFooter.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+};
+</script>
+
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Mulish:wght@500;600;700;800&display=swap');
+
 body {
+  background: url('./assets/image/background.png') no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  box-sizing: border-box;
+  color: #000;
+  font-family: 'Mulish', sans-serif;
   margin: 0;
+}
+
+a {
+  color: #2c43ad;
+  text-decoration: none;
+  white-space: nowrap;
+}
+
+#wrapper {
+  padding: 2.5rem 8rem 0 8rem;
+}
+
+#header {
+  margin: auto auto;
+  max-width: 90rem;
+  min-height: 10vh;
+}
+
+#content {
+  margin: auto auto;
+  max-width: 90rem;
+  min-height: 80vh;
+  padding-bottom: 3.5rem;
+  padding-top: 3.5rem;
+}
+
+#footer {
+  min-height: 10vh;
 }
 </style>
