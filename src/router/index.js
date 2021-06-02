1dc9fcd163bc14037f662e881ad9c8b80d99b54a
Ken So <sotszyau312@gmail.com>
2021-06-02 22:51:57 +0800

    fix: Smooth Scroll to Top issue in Safari and mobile broswer
    Signed-off-by: Ken So <sotszyau312@gmail.com>

--- src/router/index.js
+++ src/router/index.js
@@ -58,8 +58,31 @@ const router = createRouter({
   routes,
 });
 
+const smoothlyScroll = (pos, time) => {
+  const currentPos = window.pageYOffset;
+  let start = null;
+  window.requestAnimationFrame(function step(currentTime) {
+    start = !start ? currentTime : start;
+    const progress = currentTime - start;
+    if (currentPos < pos) {
+      window.scrollTo(0, (((pos - currentPos) * progress) / time) + currentPos);
+    } else {
+      window.scrollTo(0, currentPos - (((currentPos - pos) * progress) / time));
+    }
+    if (progress < time) {
+      window.requestAnimationFrame(step);
+    } else {
+      window.scrollTo(0, pos);
+    }
+  });
+};
+
 router.beforeEach((to, from, next) => {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
+  /**
+   *  Here not using window.scrollTo({ top: 0, behavior: 'smooth' })
+   *  because Safari and mobile broswer not support this feature.
+   */
+  smoothlyScroll(0, 250);
   next();
 });
 
