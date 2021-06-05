2cb38afa1156cfa3a96d58da4be391f410e1e297
Ken So <sotszyau312@gmail.com>
2021-06-06 00:49:34 +0800

    feat: Validator Localization
    Signed-off-by: Ken So <sotszyau312@gmail.com>

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,27 +1,30 @@
 import { defineRule } from 'vee-validate';
+import i18n from './i18n';
+
+const $t = i18n.global.t;
 
 function isEmail(val) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(val)) || 'Invalid email.';
+  return re.test(String(val)) || $t('validator.email_msg');
 }
 
 function isPhone(val) {
-  return /^[0-9]{8,15}$/.test(String(val)) || 'Invalid phone number.';
+  return /^[0-9]{8,15}$/.test(String(val)) || $t('validator.phone_msg');
 }
 
-defineRule('required', (val) => (val != null && val.trim() !== '') || 'It is a required field.');
+defineRule('required', (val) => (val != null && val.trim() !== '') || $t('validator.required_msg'));
 
 defineRule('email', (val) => isEmail(val));
 
 defineRule('phone', (val) => isPhone(val));
 
-defineRule('confirmed', (val, [target], ctx) => (val === ctx.form[target]) || 'Passwords must match.');
+defineRule('confirmed', (val, [target], ctx) => (val === ctx.form[target]) || $t('validator.match_msg'));
 
-defineRule('username', (val) => (isEmail(val) === true || isPhone(val) === true) || 'Invalid username, it should be email or phone number.');
+defineRule('username', (val) => (isEmail(val) === true || isPhone(val) === true) || $t('validator.username_msg'));
 
 defineRule('password', (val) => {
   const re = /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
-  return re.test(String(val)) || 'Invalid password, the password must contain at least 8 characters in length, and at least 1 lowercase, uppercase, special characters and numeric values.';
+  return re.test(String(val)) || $t('validator.password_msg');
 });
 
 defineRule('sms', async (val) => {
@@ -29,5 +32,5 @@ defineRule('sms', async (val) => {
     // call API to verify...
     return true;
   }
-  return 'The code should be a 6 digit number.';
+  return $t('validator.sms_msg');
 });
