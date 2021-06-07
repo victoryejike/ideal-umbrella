41a0680f697254fdff6e257c89658da9faa45183
Ken So <sotszyau312@gmail.com>
2021-06-07 17:07:12 +0800

    fix: i18n __INTLIFY_PROD_DEVTOOLS__ is not defined in production mode
    Signed-off-by: Ken So <sotszyau312@gmail.com>

--- vue.config.js
+++ vue.config.js
@@ -18,4 +18,15 @@ module.exports = {
       title: 'Naffiti - NFT Marketplace',
     },
   },
+  chainWebpack: (config) => {
+    config.plugin('define').tap((args) => {
+      const newArgs = [{
+        ...args,
+        __INTLIFY_PROD_DEVTOOLS__: false,
+        __VUE_I18N_FULL_INSTALL__: true,
+        __VUE_I18N_LEGACY_API__: true,
+      }];
+      return newArgs;
+    });
+  },
 };
