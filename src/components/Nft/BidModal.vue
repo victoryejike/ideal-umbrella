eac89e3b2c6128a42425100f2dc7ddc268adc8b2
dipali26 <dipalishikha26@gmail.com>
2021-06-14 22:12:38 +0530

    style: added styling in bid modal

--- src/components/Nft/BidModal.vue
+++ src/components/Nft/BidModal.vue
@@ -6,7 +6,12 @@
         src="@img/token-image.png"
       >
     </div>
-    <div> your</div>
+    <div class="bid-text">
+      <div> {{ $t('nft_details.bid.bid-content') }} </div>
+      <div class="token-description">
+        token description
+      </div>
+    </div>
   </div>
   <BaseUnderlinedInput
 
@@ -31,21 +36,39 @@
 
     :text="$t('nft_details.bid.quantity_label')"
   />
-  <div class="bidding-details">
-    <div>{{ $t('nft_details.bid.user_bidding_balance') }}</div>
-    <div>0.16 ETH</div>
-  </div>
-  <div class="bidding-details">
-    <div>{{ $t('nft_details.bid.user_balance') }}</div>
-    <div>0.16 ETH</div>
-  </div>
-  <div class="bidding-details">
-    <div>{{ $t('nft_details.bid.service_fee') }}</div>
-    <div>0.16 ETH</div>
-  </div>
-  <div class="bidding-details">
-    <div>{{ $t('nft_details.bid.total_bid_amount') }}</div>
-    <div>0.16 ETH</div>
+  <div class="details-section">
+    <div class="bidding-details">
+      <div class="label">
+        {{ $t('nft_details.bid.user_bidding_balance') }}
+      </div>
+      <div class="value">
+        0.16 ETH
+      </div>
+    </div>
+    <div class="bidding-details">
+      <div class="label">
+        {{ $t('nft_details.bid.user_balance') }}
+      </div>
+      <div class="value">
+        0.16 ETH
+      </div>
+    </div>
+    <div class="bidding-details">
+      <div class="label">
+        {{ $t('nft_details.bid.service_fee') }}
+      </div>
+      <div class="value">
+        0.16 ETH
+      </div>
+    </div>
+    <div class="bidding-details">
+      <div class="label">
+        {{ $t('nft_details.bid.total_bid_amount') }}
+      </div>
+      <div class="value">
+        0.16 ETH
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -63,22 +86,60 @@ export default {
 </script>
 <style scoped>
 .nft-box {
+  align-items: center;
   background: rgba(255, 255, 255, 0.25);
   border: 1px solid rgba(44, 67, 173, 0.25);
   border-radius: 10px;
   box-sizing: border-box;
   display: flex;
   height: 100px;
-  margin: 15px;
+  padding: 15px;
 }
 
 .token-image {
-  height: 60px;
-  width: 60px;
+  border-radius: 10px;
+  height: 70px;
+  width: 70px;
 }
 
 .bidding-details {
   display: flex;
   justify-content: space-between;
+}
+
+.details-section {
+  margin-top: 2.5rem;
+}
+
+.bid-text {
+  align-items: center;
+  color: #808080;
+  font-size: 15px;
+  font-weight: 600;
+  line-height: 26px;
+  margin-left: 20px;
+}
+
+.token-description {
+  color: #000;
+  font-weight: bold;
+}
+
+.input-field {
+  margin-top: 2rem;
+}
+
+.label {
+  color: #808080;
+  font-size: 0.875rem;
+  font-weight: 600;
+  line-height: 1.125rem;
+}
+
+.value {
+  color: rgba(0, 0, 0, 0.75);
+  font-size: 0.9375rem;
+  font-weight: bold;
+  line-height: 1.75rem;
 }
 </style>
--- src/components/UI/BaseModal.vue
+++ src/components/UI/BaseModal.vue
@@ -1,23 +1,43 @@
 <template>
-  <div class="modal-backdrop">
-    <div class="modal">
-      <header class="modal-header">
-        <slot name="header" />
-      </header>
+  <Transition name="modal-fade">
+    <div
+      class="modal-backdrop"
+      @click="close"
+    >
+      <div class="modal">
+        <header class="modal-header">
+          <slot name="header" />
+          <button
+            class="btn-close"
+            type="button"
+            @click="close"
+          >
+            x
+          </button>
+        </header>
 
-      <section class="modal-body">
-        <slot name="body" />
-      </section>
+        <section class="modal-body">
+          <slot name="body" />
+        </section>
 
-      <footer class="modal-footer">
-        <slot name="footer" />
-      </footer>
+        <footer class="modal-footer">
+          <div>
+            <slot name="footer" />
+          </div>
+        </footer>
+      </div>
     </div>
-  </div>
+  </Transition>
 </template>
 <script>
 export default {
   name: 'BaseModal',
+  emits: ['close'],
+  methods: {
+    close() {
+      this.$emit('close');
+    },
+  },
 
 };
 </script>
@@ -40,7 +60,8 @@ export default {
   display: flex;
   flex-direction: column;
   overflow-x: auto;
-  padding: 2.5rem;
+  padding-left: 2.5rem;
+  padding-right: 2.5rem;
 }
 
 .modal-header {
@@ -48,18 +69,42 @@ export default {
   font-weight: bold;
   justify-content: space-between;
   line-height: 150%;
-  margin-bottom: 1.875rem;
+  margin-bottom: 1.8rem;
+  margin-top: 1.8rem;
   position: relative;
 }
 
 .modal-footer {
   flex-direction: column;
   justify-content: flex-end;
+  margin-bottom: 2.5rem;
 }
 
 .modal-body {
   margin-bottom: 2.8125rem;
   position: relative;
+}
+
+.modal-fade-enter,
+.modal-fade-leave-to {
+  opacity: 0;
+}
+
+.modal-fade-enter-active,
+.modal-fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.btn-close {
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: bold;
+  padding: 10px;
+  position: absolute;
+  right: 0;
+  top: 0;
 }
 
 </style>
