d215000476a533e6872ca906e8a2bcd8aa0af980
Ken So <sotszyau312@gmail.com>
2021-07-03 15:33:15 +0800

    fix: SearchBar Allow Keyup Enter to Search
    Signed-off-by: Ken So <sotszyau312@gmail.com>

--- src/components/Index/SearchBar.vue
+++ src/components/Index/SearchBar.vue
@@ -14,6 +14,7 @@
           class="search-input"
           :placeholder="$t('components.search_placeholder')"
           type="text"
+          @keyup.enter="handleSearch"
         >
         <BaseRoundButton
           class="search-btn btn-primary btn-lg btn-bold"
