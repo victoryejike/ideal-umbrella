acc6667cdc179edacbd103e84ab7e72294f3c856
dipali26 <dipalishikha26@gmail.com>
2021-07-05 15:28:00 +0530

    chore: updated policy agreement content

--- src/components/Register/Agreement.vue
+++ src/components/Register/Agreement.vue
@@ -1,82 +1,224 @@
 <template>
   <div class="container">
-    <p>PLEASE READ THIS DISCLAIMER SECTION CAREFULLY.</p>
+    <p>Naffiti NFT Marketplace Terms and Condition</p>
     <p>
-      YOU SHOULD CONSULT YOUR OWN ADVISORS CONCERNING THE LEGAL, TAX,
-      ECONOMIC, FINANCIAL AND OTHER ASPECTS ASSOCIATED WITH THE FANS TOKEN PLATFORM.
+      IF ANY PROVISION OF THESE NFT TERMS OR THE NAFFITI TERMS OR ANY FUTURE
+      CHANGES ARE UNACCEPTABLE TO YOU, DO NOT USE OR CONTINUE TO USE THE NFT
+      MARKETPLACE. YOUR CONTINUED USE OF THE NFT MARKETPLACE FOLLOWING THE
+      POSTING OF ANY NOTICE OF ANY CHANGE TO THESE TERMS OF SERVICE SHALL
+      CONSTITUTE YOUR ACCEPTANCE AND AGREEMENT TO SUCH CHANGE.
+      ARBITRATION NOTICE: THE NAFFITI TERMS CONTAIN AN ARBITRATION CLAUSE.
+      EXCEPT FOR CERTAIN TYPES OF DISPUTES MENTIONED IN THAT ARBITRATION
+      CLAUSE, YOU AND COMPANY AGREE THAT DISPUTES BETWEEN US WILL BE
+      RESOLVED BY MANDATORY BINDING ARBITRATION, AND YOU AND COMPANY
+      WAIVE ANY RIGHT TO PARTICIPATE IN A CLASS-ACTION LAWSUIT OR CLASS-WIDE
+      ARBITRATION.
     </p>
     <p>
-      The FANS token does not possess any necessary characteristic required to be
-      considered a transferable security, money market instrument, unit in
-      collective investment schemes, a digital currency, commodity, security
-      or any other form of investment in any jurisdiction.
+      1. Overview of the NFT Marketplace
     </p>
     <p>
-      This white paper does not constitute a prospectus or offering document and
-      is not a solicitation for investment and does not constitute an offer of
-      financial instruments, securities to the public or a collective investment
-      scheme. Any decision to purchase FANS tokens shall be based on consideration
-      of this white paper as a whole.
+      The NFT Marketplace provides you with the opportunity to create (or as we call it, mint),
+      sell,purchase, bid on, collect, trade, showcase and otherwise transact digital blockchain
+      collectibles, which may be represented as a non-fungible token (“NFT”) linked with certain
+      digital media and art (“NFT Media”).
     </p>
     <p>
-      The Issuer reserves the right to (i) make changes to this whitepaper
-      and any documents inked to the FANS token and/or holding and use of
-      FANS token to ensure compliance with the applicable regulatory requirements
-      and (ii) , do all that is necessary to be in compliance with any regulatory
-      requirements, including but not limited to, interrupting, suspending or
-      ceasing the operations or trading of the FANS token if deemed necessary at
-      the Issuer’s sole discretion
+      We facilitate transactions between buyer and seller of an NFT, but we are not a party to any
+      agreement between buyer and seller of an NFT on the NFT Marketplace. We collect revenue on the
+      NFT Marketplace via transaction fees and other applicable fees which we display when you
+      interact with the NFT Marketplace. For the avoidance of doubt, NFTs transacted on the NFT
+      Marketplace are considered “Digital Assets” as defined in the Naffiti Terms. Company reserves
+      the right to be the final decision maker on any disputes arising from purchases via the NFT
+      Marketplace, including in connection with any auctions or other purchase methods.
     </p>
     <p>
-      It is solely up to you to ensure that no prior or subsequent approval,
-      notification, registration or licence is needed or if such is needed,
-      it is solely up to you to obtain such prior or subsequent approval,
-      notification, registration or licence or require any form in the country
-      where you are a citizen, national, resident or having a similar connecting
-      factor, or incorporated, registered or effectively managed, and the Issuer
-      shall not in any way be liable for any non-conformity of the above by you.
+      For NFT Sellers: By minting, providing, or selling an NFT through the NFT Marketplace, you
+      hereby represent and warrant that you own all legal right, title and interest in all
+      intellectual property rights to the NFT Media linked or associated with such NFT, or you are
+      legally authorized by the intellectual property owner to mint, provide or sell the NFT on the
+      NFT Marketplace. Except for NFTs minted on the NFT Marketplace, in order to list any NFT for
+      sale on the NFT Marketplace, you must first deposit the NFT for sale for custody with Company
+      until such time as the NFT is sold or you decide to remove the NFT from the NFT Marketplace.
     </p>
     <p>
-      The FANS tokens are not available to (i) a natural person being a citizen,
-      national, resident or having a similar connecting factor to; or (ii) a
-      juridical person being incorporated, registered or effectively managed
-      and controlled from or within a country, jurisdiction or territory where
-      the private placement or the holding and use of the FANS token and/or
-      virtual currency or other tokens at any other moment in time is prohibited
-      by laws, regulations or other practices and policies in the said country,
-      jurisdiction or territory, which is taken to include, but is not limited
-      to the United States of America, or any other jurisdiction where the
-      aforementioned are prohibited.
+      For clarity, Company has no obligation or liability to you for keeping, storing, or helping
+      you recover any NFT Media associated with your NFTs. For NFT Buyers: When you purchase an NFT,
+      you own the NFT that is associated with certain NFT Media, but you do not own any intellectual
+      property rights in such NFT Media except for the license grants expressly set forth herein.
+      In certain cases, we may help to evaluate or provide you with information about a seller of an
+      NFT.However, such information is provided for informational purposes only. You bear full
+      responsibility for verifying the authenticity, legitimacy, identity of any NFT you purchase on
+      the NFT Marketplace. We make no guarantees or promises about the identity, legitimacy, or
+      authenticity of any NFT on the NFT Marketplace.
+    </p>
+    <p>2. License to Your Content</p>
+    <p>
+      In connection with your use of the NFT Marketplace, you may be able to post, upload, or submit
+      content to be made available through the NFT Marketplace, including NFT Media that is tied to
+      NFTs you wish to sell on the NFT Marketplace as a seller, and any other content associated
+      with your NFTs (“Your Content”).
     </p>
     <p>
-      This white paper, the FANS tokens and/or the holding, use and trading of
-      the FANS token carries no rights, whether express or implied, other than
-      for their use on the FANSCHAIN platform and trading on cryptocurrency
-      exchanges, FANS tokens do not represent or confer any ownership right or
-      stake, share or security or equivalent rights, intellectual property rights
-      or any other form of participation relating to the Issuer.FANS tokens do
-      not give you any entitlement to acquire any such interest or entitlement in
-      respect of the Issuer.
+      You retain all rights to Your Content you post, upload, submit, or otherwise made available
+      through the NFT Marketplace, except for rights expressly granted herein. In order to operate
+      the NFT Marketplace, we must obtain from you certain license rights in Your Content so that
+      actions we take in operating the NFT Marketplace are not considered legal violations.
     </p>
     <p>
-      The administrator of the Issuer, as identified in the Considerations section
-      of this white paper, are the persons responsible for the information
-      contained in this whitepaper. To the best of the knowledge and belief of
-      the administrators of the Issuer (who have all taken reasonable care to
-      ensure such is the case), the information contained in this white paper
-      is in accordance with the facts and does not omit anything likely to
-      affect the import of such information. The administrators of the Issuer
-      accept responsibility accordingly.
+      Accordingly, by using the NFT Marketplace and uploading Your Content or otherwise made Your
+      Content available, you grant us a license to access, use, host, cache, store, copy, reproduce,
+      transmit, display, publish, distribute, adapt and modify (for technical purposes,
+      e.g., making sure content is viewable on smartphones as well as computers and other devices)
+      Your Content in any and all media or distribution methods (now know or later developed) but
+      solely as required to be able to operate and provide services of the NFT Marketplace.
     </p>
     <p>
-      Persons located in the following jurisdictions may not apply for or receive
-      Tokens: Albania, Barbados, Botswana, Cambodia, Cuba, Democratic People's
-      Republic of Korea, Democratic Republic of the Congo, Ghana, Jamaica, Iraq,
-      Iran, Mauritius, Myanmar, Libya, Pakistan, Nicaragua, Panama, the Russian
-      Federation, Somalia, South Sudan, Sudan, Syria, the People's Republic of
-      China (including the Hong Kong Special Administrative Area), Trinidad
-      and Tobago, Uganda, The United States of America, Vanuatu, Yemen, and
-      Zimbabwe.
+      You agree that this license includes the right for us to provide, promote, and improve the NFT
+      Marketplace and to make Your Content available to other companies, organizations or
+      individuals for the distribution, promotion or publication of Your Content on other media and
+      services. You agree that these rights and licenses are royalty free, transferable,
+      sub-licensable, worldwide and irrevocable (for so long as Your Content is stored with us),
+      and include a right for us to make Your Content available to, and pass these rights along to,
+      others with whom we have contractual relationships related to the provision of the NFT
+      Marketplace,and solely for purpose of providing the NFT Marketplace, and to otherwise permit
+      access to disclose Your Content to third parties if we determine such access is necessary to
+      comply with our legal obligations.
+    </p>
+    <p>
+      As part of the foregoing license grant you agree that the other Users of the NFT Marketplace
+      shall have the right to comment on and/or tag Your Content and/or to use, publish, display,
+      modify or include a copy of Your Content as part of their own use of the NFT Marketplace;
+      except that the foregoing shall not apply to any of Your Content that you post privately for
+      non-public display on the NFT Marketplace.
+    </p>
+    <p>
+      By posting or submitting Your Content to the NFT Marketplace, you represent and warrant that
+      you have, or have obtained, all rights, licenses, consents, permissions, power and/or
+      authority necessary to grant the rights granted herein for Your Content. You agree that Your
+      Content will not contain material subject to copyright or other proprietary rights, unless you
+      have the necessary permission or are otherwise legally entitled to post the material and to
+      grant us the license described above.
+    </p>
+    <p>
+      If you sell an NFT through the NFT Marketplace, you grant to the buyer of the NFT a worldwide,
+      non-exclusive, non-transferable, royalty-free license to use, copy, and display the NFT Media
+      for such purchased NFT, solely for the following purposes: (a) for the buyer’s own personal
+      use; (b) as part of a marketplace that permits the purchase and sale of such NFTs, provided
+      that the marketplace cryptographically verifies each NFT’s owner’s rights to display the NFT
+      Media for their NFTs to ensure that only the actual owner can display the NFT Media; or (c)
+      as part of a third party website or application that permits the inclusion, involvement, or
+      participation of your NFT, provided that the website/application cryptographically verifies
+      each NFT’s owner’s rights to display the NFT Media for their NFTs to ensure that only the
+      actual owner can display the NFT Media, and provided that the NFT Media is no longer visible
+      once the owner of the NFTs leaves the website/application (the “NFT Purchase License”).
+    </p>
+    <p>
+      If you are a buyer of NFTs, then you acknowledge and agree that the NFT Purchase License set
+      forth above only lasts as long as you are the valid owner and holder of the NFT associated
+      with the licensed NFT Media. If you sell or transfer the NFT to another person, this NFT
+      Purchase License will transfer to such other owner or holder of the NFT, and you will no
+      longer have the benefits of such NFT Purchase License.
+    </p>
+    <p>
+      Unless otherwise specified by seller of an NFT in writing, your purchase of an NFT does not
+      give you the right to publicly display, perform, distribute, sell, or otherwise reproduce the
+      NFT or its related NFT Media for any commercial purpose. If you sell an NFT, you agree that
+      you will not have any claims against Company for any breach of these NFT Terms by a purchaser,
+      including if they make commercial use of the related NFT Media in breach of these NFT Terms.
+    </p>
+    <p>
+      We have the right to remove or refuse to post any of Your Content, including NFTs, (a) for any
+      or no reason in our sole discretion; and (b) take any action with respect to Your Content that
+      we deem necessary or appropriate in our sole discretion, including if we believe that Your
+      Content violates these NFT Terms, infringes any intellectual property right of any person or
+      entity, threatens the personal safety of Users of the NFT Marketplace or the public, or could
+      create liability for Company or other Users.
+    </p>
+    <p>
+      3. Copyright Policy
+    </p>
+    <p>
+      Company may, but is not obligated to, monitor the NFTs, NFT Media and Your Content uploaded to
+      the NFT Marketplace for any infringement of a third party’s intellectual property rights.
+      However,Company cannot undertake to review all such content before it is posted on the
+      Service, and cannot ensure prompt removal of objectionable content after it has been posted.
+      Accordingly, Company assume no liability for any action regarding transmissions,
+      communications, or content provided by any user or third party.
+    </p>
+    <p>
+      If you believe that any text, graphics, photos, audio, videos or other materials or works
+      uploaded,downloaded or appearing on the Services have been copied in a way that constitutes
+      copyright infringement, you may submit a notification to our legal department, by providing
+      the following information in writing: identification of the copyrighted work that is claimed
+      to be infringed;identification of the allegedly infringing material that is requested to be
+      removed,including a description of where it is located on the NFT Marketplace.
+    </p>
+    <p>
+      Information for our copyright agent to contact you, such as an address, telephone number and
+      e-mail address; a statement that you have a good faith belief that the identified, allegedly
+      infringing use is not authorized by the copyright owners, its agent or the law; a statement
+      that the information above is accurate, and under penalty of perjury, that you are the
+      copyright owner or the authorized person to act on behalf of the copyright owner; and the
+      physical or electronic signature of a person authorized to act on behalf of the owner of the
+      copyright or of an exclusive right that is allegedly infringed.
+    </p>
+    <p>
+      It is our policy, in appropriate circumstances and at our discretion, to disable or terminate
+      the accounts of Users who repeatedly infringe copyrights or intellectual property rights of
+      others.Any User of the NFT Marketplace who has uploaded or posted materials identified as
+      infringing as described above may supply a counter-notification. When we receive a
+      counter-notification, we may reinstate the posts or material in question, in our
+      sole discretion.
+    </p>
+    <p>
+      4. Verification and Payment
+    </p>
+
+    <p>
+      When you make purchases through the NFT Marketplace, including, without limitation, any
+      purchase for NFTs, you must provide and maintain valid payment information in connection
+      with your Naffiti Account (as defined in the Naffiti Terms) with us. You represent and warrant
+      that you are authorized to use the payment method you use via the NFT Marketplace to make any
+      purchase. You authorize us to charge your payment method for the total amount of your purchase
+      price.Your order may be suspended or cancelled for any reason, including if the payment
+      method cannot be verified, is invalid or is otherwise not acceptable. Other payments terms
+      for purchases are set forth in the relevant sections of the Naffiti Terms. We have no
+      liability to you or to any third party for any claims or damages that may arise as a result of
+      any payments or transactions that you engage in via the NFT Marketplace, or any other payment
+      or transactions that you conduct via the NFT Marketplace. We do not provide refunds for any
+      purchases that you might make on or through the NFT Marketplace – whether for NFTs or anything
+      else.You will be solely responsible to pay any and all sales, use, value-added and other
+      taxes,duties, and assessments (except taxes on our net income) now or hereafter
+      claimed or imposed by any governmental authority associated with your use of the NFT
+      Marketplace or NFTs, except for income taxes levied on us as a result of such purchases of
+      NFTs.
+    </p>
+    <p>
+      5. Assumption of Risks
+    </p>
+    <p>
+      Any purchase or sale you make, accept or facilitate outside of the NFT Marketplace of an
+      NFT will be entirely at your risk. You acknowledge that you have obtained sufficient
+      information to make an informed decision to purchase an NFT, including carefully reviewing
+      the code of the smart contract and the NFT and fully understand and accept the functions
+      of the same. We do not control or endorse purchases or sales of NFTs outside of the
+      NFT Marketplace.
+    </p>
+    <p>
+      We expressly deny and disclaim any liability to you and deny any obligation to indemnify you
+      or hold you harmless for any losses you may incur by transacting, or facilitating
+      transactions,in NFTs outside of the NFT Marketplace. Certain parts of the NFT Marketplace
+      may display, include or make available content, data, information,applications or materials
+      from third parties (“Third Party Materials”).
+    </p>
+    <p>
+      By using the NFT Marketplace, you acknowledge and agree that Company is not responsible for
+      examining or evaluating the content, accuracy, completeness, availability, timeliness,
+      validity,copyright compliance, legality, decency, quality or any other aspect of such
+      Third Party Materials. We do not warrant or endorse and do not assume and will not have any
+      liability or responsibility to you or any other person for any third-party services,
+      Third Party Materials, or for any other materials,products, or services of third parties.
     </p>
   </div>
 </template>
