d808370ffc937d11451e0e451931ac115f4ee252
Ken So <sotszyau312@gmail.com>
2021-08-11 00:26:17 +0800

    chore: Freeze Enums Object + Add Missed Tranlation
    Signed-off-by: Ken So <sotszyau312@gmail.com>

--- src/utils/enums.js
+++ src/utils/enums.js
@@ -1,13 +1,13 @@
-export const OtpType = {
+export const OtpType = Object.freeze({
   PASSWORD_RESET: 'PASSWORD_RESET',
   FORGOT_PASSWORD: 'FORGOT_PASSWORD',
   REBIND_PHONE: 'REBIND_PHONE',
   REBIND_EMAIL: 'REBIND_EMAIL',
   REGISTER: 'REGISTER',
-};
+});
 
-export const PriceType = {
+export const PriceType = Object.freeze({
   FIXED: 'fixed',
   TIMED_AUCTION: 'timed_auction',
   UNLIMITED_AUCTION: 'unlimited_auction',
-};
+});
